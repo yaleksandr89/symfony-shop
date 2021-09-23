cc37113207bc2bf53336c50f664fba73e94a37fb
Aleksandr Yurchenko <y.aleksandr89@gmail.com>
2021-09-24 01:31:27 +0300

    added static (html, css, js) and changed structure templates

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends AbstractController
+{
+    #[Route('/', name: 'homepage')]
+    public function index(): Response
+    {
+        return $this->render('front/default/index.html.twig');
+    }
+}
