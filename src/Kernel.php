4dfd3f49ef4a53d8ca343037800169b64d172b8f
Aleksandr Yurchenko <y.aleksandr89@gmail.com>
2021-09-24 00:48:31 +0300

    styling code to PSR12

--- src/Kernel.php
+++ src/Kernel.php
@@ -7,6 +7,8 @@ use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigura
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
+use function dirname;
+
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
@@ -14,11 +16,11 @@ class Kernel extends BaseKernel
     protected function configureContainer(ContainerConfigurator $container): void
     {
         $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
+        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
+        if (is_file(dirname(__DIR__) . '/config/services.yaml')) {
             $container->import('../config/services.yaml');
-            $container->import('../config/{services}_'.$this->environment.'.yaml');
+            $container->import('../config/{services}_' . $this->environment . '.yaml');
         } else {
             $container->import('../config/{services}.php');
         }
@@ -26,10 +28,10 @@ class Kernel extends BaseKernel
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
         $routes->import('../config/{routes}/*.yaml');
 
-        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
+        if (is_file(dirname(__DIR__) . '/config/routes.yaml')) {
             $routes->import('../config/routes.yaml');
         } else {
             $routes->import('../config/{routes}.php');
