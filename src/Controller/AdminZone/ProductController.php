63b2a59684f5666120fa07576921ed580cff9730
Aleksandr Yurchenko <y.aleksandr89@gmail.com>
2021-09-30 23:26:29 +0300

    #6 added ProductController, template list

---
+++ src/Controller/AdminZone/ProductController.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\AdminZone;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/product", name="admin_product_")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list(ProductRepository $productRepository): Response
+    {
+        //$products = $productRepository->findAll();
+        $products = $productRepository->findBy(['isDeleted' => false], ['id' => 'DESC'], 50);
+
+        return $this->render('admin/product/list.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit")
+     * @Route("/add", name="add")
+     */
+    public function edit(): Response
+    {
+        // ...
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete")
+     */
+    public function delete(): Response
+    {
+        // ...
+    }
+}
