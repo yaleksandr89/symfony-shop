3512dac4901f4d8abe8e3034f5c0dda40919b1d0
Aleksandr Yurchenko <y.aleksandr89@gmail.com>
2021-10-01 13:29:23 +0300

    #6 added functionality: add,edit product (+template). Del uneccessary code and template

--- src/Controller/AdminZone/ProductController.php
+++ src/Controller/AdminZone/ProductController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Controller\AdminZone;
 
+use App\Entity\Product;
+use App\Form\EditProductFormType;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,13 +17,31 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ProductController extends AbstractController
 {
+    // >>> Autowiring
+    /**
+     * @var ProductRepository
+     */
+    private ProductRepository $productRepository;
+
+    /**
+     * @required
+     * @param ProductRepository $productRepository
+     * @return ProductController
+     */
+    public function setProductRepository(ProductRepository $productRepository): ProductController
+    {
+        $this->productRepository = $productRepository;
+        return $this;
+    }
+    // Autowiring <<<
+
     /**
      * @Route("/list", name="list")
      */
     public function list(ProductRepository $productRepository): Response
     {
-        //$products = $productRepository->findAll();
-        $products = $productRepository->findBy(['isDeleted' => false], ['id' => 'DESC'], 50);
+        //$products = $this->productRepository->findAll();
+        $products = $this->productRepository->findBy(['isDeleted' => false], ['id' => 'DESC'], 50);
 
         return $this->render('admin/product/list.html.twig', [
             'products' => $products
@@ -31,9 +52,24 @@ class ProductController extends AbstractController
      * @Route("/edit/{id}", name="edit")
      * @Route("/add", name="add")
      */
-    public function edit(): Response
+    public function edit(Request $request, Product $product = null): Response
     {
-        // ...
+        $form = $this->createForm(EditProductFormType::class, $product);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('admin_product_edit', ['id' => $product->getId()]);
+        }
+
+        return $this->render('admin/product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
     }
 
     /**
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,14 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Product;
-use App\Form\EditProductFormType;
-use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,58 +21,6 @@ class DefaultController extends AbstractController
         $productList = $em->getRepository(Product::class)->findAll();
         return $this->render('front/default/index.html.twig', [
             'productList' => $productList
-        ]);
-    }
-
-    /**
-     * @throws Exception
-     * @Route("/product-add_old", name="product_add_old", methods="GET")
-     */
-    public function productAdd(): Response
-    {
-        $product = new Product();
-        $product->setTitle('Product ' . random_int(1, 100));
-        $product->setDescription('desc product ...');
-        $product->setPrice('10');
-        $product->setQuantity(1);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($product);
-        $em->flush();
-
-        return $this->redirectToRoute('main_homepage');
-    }
-
-    /**
-     * @Route("/product-edit/{id}", name="product_edit", methods={"GET","POST"}, requirements={"id"="\d+"})
-     * @Route("/product-add", name="product_add", methods={"GET","POST"})
-     * @param Request $request
-     * @param int|null $id
-     * @return Response|null
-     */
-    public function editProduct(Request $request, int $id = null): ?Response
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        if ($id) {
-            $product = $em->getRepository(Product::class)->find($id);
-        } else {
-            $product = new Product();
-        }
-
-        $form = $this->createForm(EditProductFormType::class, $product);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em->persist($product);
-            $em->flush();
-
-            return $this->redirectToRoute('product_edit', ['id' => $product->getId()]);
-        }
-
-        return $this->render('front/default/edit_product.html.twig', [
-            'form' => $form->createView(),
         ]);
     }
 }
