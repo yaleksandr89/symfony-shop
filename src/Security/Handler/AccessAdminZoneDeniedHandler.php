2571209651df4817fd5a5255fcc7e3165211032a
Aleksandr Yurchenko <y.aleksandr89@gmail.com>
2021-10-02 20:35:14 +0300

    hotfix-2 realised: 'access_denied_handler' / 'entry_point'

---
+++ src/Security/Handler/AccessAdminZoneDeniedHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security\Handler;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessAdminZoneDeniedHandler implements AccessDeniedHandlerInterface
+{
+    /**
+     * @var UrlGeneratorInterface
+     */
+    private UrlGeneratorInterface $urlGenerator;
+
+    /**
+     * @param UrlGeneratorInterface $urlGenerator
+     */
+    public function __construct(UrlGeneratorInterface $urlGenerator)
+    {
+        $this->urlGenerator = $urlGenerator;
+    }
+
+    /**
+     * @param Request $request
+     * @param AccessDeniedException $accessDeniedException
+     * @return Response|null
+     */
+    public function handle(Request $request, AccessDeniedException $accessDeniedException): ?Response
+    {
+        $request->getSession()->getFlashBag()->add('warning', 'You have to login in order to access this page.');
+        return new RedirectResponse($this->urlGenerator->generate('admin_security_login'));
+    }
+}
