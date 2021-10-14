e045b7516d9a84f223576efcb0258cc3b3766184
Aleksandr Yurchenko <y.aleksandr89@gmail.com>
2021-10-14 11:39:22 +0300

    install vue, creating demo 'template'

---
+++ assets/js/section/admin/admin-order/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Vue app: {{ productsCount }}
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    productsCount: () => {
+      return 123;
+    }
+  }
+}
+</script>
