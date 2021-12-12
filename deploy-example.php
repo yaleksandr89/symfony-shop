6e477eecbbe27bf1d218964915f34a8a74022c29
Aleksandr Yurchenko <y.aleksandr89@gmail.com>
2021-12-12 21:47:27 +0300

    fix deploy.php for the wirking with Symfony 5.3

--- deploy-example.php
+++ deploy-example.php
@@ -1,76 +1,104 @@
 <?php
 
+// Testing - Symfony 5.3
+
 namespace Deployer;
 
 require 'recipe/symfony.php';
 
 set('ssh_type', 'native');
 set('ssh_multiplexing', true);
-set('bin/console', '{{bin/php}} {{release_path}}/bin/console');
+set('allow_anonymous_stats', false);
+set('env', []); // Run command environment (for example, SYMFONY_ENV=prod)
+set('shared_dirs', []);
+set('shared_files', []);
+set('writable_dirs', []);
+set('bin_dir', 'bin');
+set('var_dir', 'var');
 
 // Project name
-set('application', '[Pet project] Symfony shop');
+set('application', 'Pet project - Symfony shop');
 
 // Project repository
-set('repository', 'git@github.com:yaleksandr89/symfony-shop.git');
+set('repository', '......');
 
-set('composer_options', '{{composer_actions}} --verbose --prefer-dist --no-progress --no-interaction --optimize-autoload --no-script');
+set(
+    'composer_options',
+    '{{composer_action}}  --verbose --prefer-dist --no-progress --no-interaction --optimize-autoloader --no-scripts'
+);
 
-set('keep_release', 3);
+set('keep_releases', 3);
 
 // Shared files/dirs between deploys
-add('shared_files', ['.env']);
-add('shared_dirs', ['var/log', 'public/uploads']);
+add('shared_files', [
+    '.env.local.php',
+]);
+
+add('shared_dirs', [
+    'var/log',
+    'var/cache',
+    'public/uploads',
+]);
 
 // Writable dirs by web server
-add('writable_dirs', ['var', 'public/uploads']);
+add('writable_dirs', [
+    'var',
+    'public/uploads',
+]);
 
-set('allow_anonymous_stats', false);
+// Предварительно сгенерировать файл: "composer dump-env prod"
+task('upload:env', function () {
+    upload('.env.local.php', '{{deploy_path}}/shared/.env.local.php');
+})->desc('Environment setup');
 
 // Hosts
-host('..............')
-    ->hostname('..............')
-    ->port(22)
-    ->user('..............')
-    ->identityFily('..............')
-    ->forwardAgent(true)
-    ->multiplexing(true)
+host('......')
     ->stage('production')
-    ->set('branch', 'master')
-    ->set('deploy_path', '..............');
-
-task('pwd', function (): void {
-    $result = run('pwd');
-    writeln("Current dir: {$result}");
-});
-
-set('env', function (): array {
-    return [
-        'APP_ENV' => 'prod',
-        'DATABASE_URL' => '..............',
-        'COMPOSER_MEMORY_LIMIT' => '1024M',
-    ];
-});
+    ->user('......')
+    ->set('deploy_path', '......');
 
 // Tasks
-task('deploy:build:assets', function () {
-    run('npm install');
-    run('npm run build');
+task('deploy:assets:install', function () {
+    run('{{bin/console}} assets:install {{console_options}} {{release_path}}/public');
+})->desc('Install bundle assets');
+
+task('deploy:npm:install', function () {
+    run('cd {{release_path}} && npm install');
 });
 
-/*
- * Если скрипты собираются локально
-task('deploy:build_local:assets', function () {
-    upload('./public/build', '{{release_path}}/public/.');
-    upload('./public/bundles', '{{release_path}}/bundles/.');
+task('deploy:npm:build', function () {
+    run('cd {{release_path}} && npm run build');
 });
- */
 
-alert('deploy:update_code', 'deploy:build:assets');
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
 
 // Migrate database before symlink new release.
+before('deploy:symlink', 'database:migrate');
 
-before('deploy', 'success');
-
+/*
+ * Main task
+ */
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'upload:env',
+    'deploy:update_code',
+    'deploy:clear_paths',
+    'deploy:create_cache_dir',
+    'deploy:shared',
+    'deploy:assets',
+    'deploy:vendors',
+    'deploy:assets:install',
+    'deploy:npm:install',
+    'deploy:npm:build',
+    'deploy:assetic:dump',
+    'deploy:cache:clear',
+    'deploy:cache:warmup',
+    'deploy:writable',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+])->desc('Deploy your project');
