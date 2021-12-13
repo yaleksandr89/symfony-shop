89c0fd89099b46da078cf63f1ee4d3ba818a436f
Aleksandr Yurchenko <y.aleksandr89@gmail.com>
2021-12-13 12:28:27 +0300

    added generate robots.txt and allow page product by slug

--- src/Controller/Front/ProductController.php
+++ src/Controller/Front/ProductController.php
@@ -5,23 +5,43 @@ declare(strict_types=1);
 namespace App\Controller\Front;
 
 use App\Entity\Product;
+use Doctrine\DBAL\Types\ConversionException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
 {
     /**
-     * @Route("/product/{uuid}", name="main_product_show")
+     * @Route("/product/{identifier}", name="main_product_show")
      * @Route("/product", name="main_product_show_blank")
      *
-     * @param Product|null $product
+     * @param string $identifier
      *
      * @return Response
      */
-    public function show(Product $product = null): Response
+    public function show(string $identifier): Response
     {
+        try {
+            $product = $this->getDoctrine()
+                ->getRepository(Product::class)
+                ->findOneBy(['uuid' => $identifier]);
+        } catch (ConversionException $e) {
+            $product = null;
+        }
+
+        if (!$product) {
+            try {
+                $product = $this->getDoctrine()
+                    ->getRepository(Product::class)
+                    ->findOneBy(['slug' => $identifier]);
+            } catch (ConversionException $e) {
+                $product = null;
+            }
+        }
+
         if (!$product) {
             throw new NotFoundHttpException();
         }
@@ -32,8 +52,11 @@ class ProductController extends AbstractController
             return $this->redirectToRoute('main_homepage');
         }
 
+        $canonicalLink = $this->generateUrl('main_product_show', ['identifier' => $product->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL);
+
         return $this->render('front/product/show.html.twig', [
             'product' => $product,
+            'canonicalLink' => $canonicalLink,
         ]);
     }
 }
---
+++ src/Controller/Front/RobotsTxtController.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Front;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use Doctrine\Persistence\ManagerRegistry as Doctrine;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class RobotsTxtController extends AbstractController
+{
+    // >>> Autowiring
+    /**
+     * @var Doctrine
+     */
+    private Doctrine $doctrine;
+
+    /**
+     * @required
+     *
+     * @param Doctrine $doctrine
+     *
+     * @return self
+     */
+    public function setDoctrine(Doctrine $doctrine): self
+    {
+        $this->doctrine = $doctrine;
+
+        return $this;
+    }
+    // Autowiring <<<
+
+    /**
+     * @Route("/robots.txt", name="main_robots.txt")
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('front/robots.txt.twig', [
+            'activeCategories' => $this->getActiveCategories(),
+            'activeProducts' => $this->getActiveProducts(),
+        ]);
+    }
+
+    /**
+     * @return array
+     */
+    private function getActiveCategories(): array
+    {
+        $activeCategory = [];
+        $categories = $this->doctrine
+            ->getRepository(Category::class)
+            ->findBy(['isDeleted' => false]);
+
+        foreach ($categories as $category) {
+            if ($category->getProducts()->count() > 0) {
+                $preparingLink = explode('/', $this->generateUrl('main_category_show', ['slug' => $category->getSlug()]));
+                $preparingLink[1] = '*';
+                $preparingLink = implode('/', $preparingLink);
+
+                $activeCategory[] = $preparingLink;
+            }
+        }
+
+        return $activeCategory;
+    }
+
+    /**
+     * @return array
+     */
+    private function getActiveProducts(): array
+    {
+        $activeProduct = [];
+        $products = $this->doctrine
+            ->getRepository(Product::class)
+            ->findBy([
+                'isDeleted' => false,
+                'isPublished' => true,
+            ]);
+
+        foreach ($products as $product) {
+            $activeProduct[] = $this->generateUrl('main_product_show', ['identifier' => $product->getSlug()], UrlGeneratorInterface::ABSOLUTE_URL);
+        }
+
+        return $activeProduct;
+    }
+}
