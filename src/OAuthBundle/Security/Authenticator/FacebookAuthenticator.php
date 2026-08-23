6e72554af0bad04cd2759a55a9953281fab0f6fd
Aleksandr Yurchenko <y.aleksandr89@yandex.ru>
2026-08-23 15:50:39 +0300

    refactor: централизовать общий OAuth callback-поток

--- src/OAuthBundle/Security/Authenticator/FacebookAuthenticator.php
+++ src/OAuthBundle/Security/Authenticator/FacebookAuthenticator.php
@@ -7,45 +7,17 @@ namespace App\OAuthBundle\Security\Authenticator;
 use App\Entity\User;
 use App\OAuthBundle\Factory\UserFactory;
 use App\OAuthBundle\Provider\Facebook\FacebookUser;
-use App\OAuthBundle\Security\OAuth\OAuthCallbackModeResolver;
-use App\OAuthBundle\Security\OAuth\OAuthLoginHandler;
 use App\OAuthBundle\Security\OAuth\OAuthProvider;
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
-use Symfony\Contracts\Service\Attribute\Required;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 final class FacebookAuthenticator extends AbstractOAuthAuthenticator
 {
-    private OAuthCallbackModeResolver $callbackModeResolver;
-
-    public function __construct(
-        private readonly ClientRegistry $clientRegistry,
-        private readonly OAuthLoginHandler $oauthLoginHandler,
-        private readonly RouterInterface $router,
-        private readonly TranslatorInterface $translator,
-    ) {
-    }
-
-    #[Required]
-    public function setCallbackModeResolver(OAuthCallbackModeResolver $callbackModeResolver): void
-    {
-        $this->callbackModeResolver = $callbackModeResolver;
-    }
-
     public function supports(Request $request): bool
     {
-        return 'connect_facebook_check' === $request->attributes->get('_route')
-            && $this->callbackModeResolver->useOrdinaryAuthenticator();
+        return $this->supportsOrdinaryCallback($request, 'connect_facebook_check');
     }
 
     public function authenticate(Request $request): Passport
@@ -65,19 +37,5 @@ final class FacebookAuthenticator extends AbstractOAuthAuthenticator
                 static fn (): User => UserFactory::createUserFromFacebook($facebookUser, (string) $email),
             );
         }));
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): RedirectResponse
-    {
-        return new RedirectResponse($this->router->generate('main_profile_index'));
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
-    {
-        /** @var Session $session */
-        $session = $request->getSession();
-        $session->getFlashBag()->add('danger', $this->translator->trans('oauth.authentication.failure'));
-
-        return new RedirectResponse($this->router->generate('main_login', ['_locale' => $request->getLocale()]));
     }
 }
--- src/OAuthBundle/Security/Authenticator/GithubEnAuthenticator.php
+++ src/OAuthBundle/Security/Authenticator/GithubEnAuthenticator.php
@@ -6,46 +6,18 @@ namespace App\OAuthBundle\Security\Authenticator;
 
 use App\Entity\User;
 use App\OAuthBundle\Factory\UserFactory;
-use App\OAuthBundle\Security\OAuth\OAuthCallbackModeResolver;
-use App\OAuthBundle\Security\OAuth\OAuthLoginHandler;
 use App\OAuthBundle\Security\OAuth\OAuthProvider;
-use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
 use League\OAuth2\Client\Provider\GithubResourceOwner;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\Session;
-use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
-use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
-use Symfony\Contracts\Service\Attribute\Required;
-use Symfony\Contracts\Translation\TranslatorInterface;
 
 class GithubEnAuthenticator extends AbstractOAuthAuthenticator
 {
-    public function __construct(
-        private ClientRegistry $clientRegistry,
-        private OAuthLoginHandler $oauthLoginHandler,
-        private RouterInterface $router,
-        private TranslatorInterface $translator,
-    ) {
-    }
-
-    private OAuthCallbackModeResolver $callbackModeResolver;
-
-    #[Required]
-    public function setCallbackModeResolver(OAuthCallbackModeResolver $callbackModeResolver): void
-    {
-        $this->callbackModeResolver = $callbackModeResolver;
-    }
-
     public function supports(Request $request): ?bool
     {
-        return 'connect_github_en_check' === $request->attributes->get('_route')
-            && $this->callbackModeResolver->useOrdinaryAuthenticator();
+        return $this->supportsOrdinaryCallback($request, 'connect_github_en_check');
     }
 
     public function authenticate(Request $request): Passport
@@ -67,24 +39,5 @@ class GithubEnAuthenticator extends AbstractOAuthAuthenticator
                 );
             })
         );
-    }
-
-    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): RedirectResponse
-    {
-        $targetUrl = $this->router->generate('main_profile_index');
-
-        return new RedirectResponse($targetUrl);
-    }
-
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): Response
-    {
-        /** @var Session $session */
-        $session = $request->getSession();
-        $session->getFlashBag()->add(
-            'danger',
-            $this->translator->trans('oauth.authentication.failure')
-        );
-
-        return new RedirectResponse($this->router->generate('main_login', ['_locale' => $request->getLocale()]));
     }
 }
